25bbfbc1b502e24043d3190966c251b92e673c01
hninwutyikyaw <hninwutyikyaw.26@gmail.com>
2023-02-25 01:41:48 +0630

    add count feature

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const inputText = document.querySelector("#inputText");
 const addButton = document.querySelector("#addButton");
 const lists = document.querySelector("#lists");
+const doneCount = document.querySelector("#doneCount");
+const totalCount = document.querySelector("#totalCount");
+const primaryLists = [
+  "Go to the GYM",
+  "Drink 2L of water",
+  "Do exercise",
+  "Practice Typing",
+];
 
 //create function because of reuseable
 const createLi = (text) => {
@@ -22,14 +30,29 @@ const createLi = (text) => {
     ${text}
   </label>
 </div>
-<div>
-<button type="button" class="btn btn-warning btn-sm" onclick="edit(event)">
-  edit
+<div class="btn-group">
+<button type="button" class="btn btn-outline-dark btn-sm" onclick="edit(event)">
+  <i class="bi bi-pencil pe-none"></i>
 </button>
-<button type="button" class="btn btn-danger btn-sm" onclick="removeList(event)">
-  del
-</button></div>`;
+<button type="button" class="btn btn-outline-dark btn-sm" onclick="removeList(event)">
+  <i class="bi bi-trash pe-none"></i>
+</button>
+</div>`;
   return li;
+};
+
+primaryLists.forEach((primaryList) => {
+  lists.append(createLi(primaryList));
+});
+
+const counter = () => {
+  totalCount.innerText = lists.children.length;
+  // const arr = [...lists.children];
+  const arr = Array.from(lists.children);
+  const done = arr.filter(
+    (el) => el.querySelector(".form-check-input").checked === true
+  ).length;
+  doneCount.innerText = done;
 };
 
 const addList = () => {
@@ -37,6 +60,7 @@ const addList = () => {
   if (inputText.value.trim()) {
     lists.append(createLi(inputText.value));
     inputText.value = null;
+    counter();
   } else {
     alert("please fill the input field");
   }
@@ -52,7 +76,9 @@ inputText.addEventListener("keyup", (event) => {
 //remove list
 const removeList = (event) => {
   if (confirm("Are you sure want to delete")) {
-    event.target.parentElement.parentElement.remove();
+    // event.target.parentElement.parentElement.parentElement.remove();
+    event.target.closest("li").remove();
+    counter();
   }
 };
 
@@ -61,13 +87,19 @@ const done = (event) => {
   event.target.nextElementSibling.classList.toggle(
     "text-decoration-line-through"
   );
+  counter();
 };
 
 //edit
 const edit = (event) => {
-  // console.dir(event.target.parentElement);
-  const old =
-    event.target.parentElement.parentElement.querySelector(".form-check-label");
+  const old = event.target.closest("li").querySelector(".form-check-label");
   const newText = prompt("Input new", old.innerText);
-  old.innerText = newText;
+  if (newText && newText.trim()) {
+    old.innerText = newText;
+  }
+  // event.target.parentElement.parentElement.querySelector(".form-check-label"); complexity
+};
+
+window.onload = (event) => {
+  counter();
 };
